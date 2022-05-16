5d4f0632a78232e54af8396eb86d0e5d5b70bcba
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2022-05-16 21:32:18 +0530

    Update index.php

--- index.php
+++ index.php
@@ -468,8 +468,8 @@
                                         </div>
                                 
                                 <div class="m-2 bg-white border rounded">
-                                            <img src="vender/files/notices/img_1.png">
-                                            <p class="p-2 mb-0 font-title text-data"><small>Prof. Gauri Phadatare, Dr. P. A. Deshmukh & Dr. U. G. Potdar received a grand of Rs. 12,14,406 from AICTE Quality Improvement Schemes (AQIS) for the title "Utilization of Green-clean energy source for the development of a novel hybrid line and point focused solar collector system for biomedical steam sterilization application."</b></small>
+                                            <img src="vender/files/notices/new_img_ach.jpg">
+                                            <p class="p-2 mb-0 font-title text-data"><small>Prof. Gauri Phadatare, Dr. P. A. Deshmukh & Dr. U. G. Potdar received <br>a grand of Rs. 12,14,406 from AICTE Quality Improvement Schemes (AQIS)<br> for the title "Utilization of Green-clean energy source for the development of a novel hybrid line and point <br> focused solar collector system for biomedical steam sterilization application."</b></small>
                                             </p>
                                         </div>
                                     
