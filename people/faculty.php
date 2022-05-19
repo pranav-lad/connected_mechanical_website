cbfbe623dadda8bb4e5110cbbac126c240b8447b
Pranav Lad <87913792+pranav-lad@users.noreply.github.com>
2022-05-19 11:30:18 +0530

    Update faculty.php

--- people/faculty.php
+++ people/faculty.php
@@ -1065,7 +1065,7 @@
                     <!-- -----------------jaya goyal------------------  -->
                 <div class="row bg-white mx-lg-5 mx-md-3 mx-sm-1 mt-3 rounded">
                     <div class="col-sm-3 p-2 text-center align-self-center container-fluid">
-                        <img src="../faculty photo/jaya goyal.jpg" class="card-img" style="width: 150px;" alt="Faculty Image"> 
+                        <img src="../faculty photo/kishor_sir.jpg" class="card-img" style="width: 150px;" alt="Faculty Image"> 
                         <br/> <button class="website btn btn-primary mt-3"> <a href="#" target="_blank"><span class="text-white">Personal Website</span></a> </button>
                     </div>
                     <div class="col-sm-9 align-self-center pt-3">
